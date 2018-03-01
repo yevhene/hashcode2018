91c22136e93ee5a382c1d0b2be805decf77d1603
Paul Druziak <paul.druziak@icloud.com>
2018-03-01 20:53:04 +0200

    can ride?

--- car.rb
+++ car.rb
@@ -1,10 +1,19 @@
 class Car
-  attr_reader :x, :y
-  def initialize(x, y)
-    @x, @y = x, y
+  attr_reader :row, :column
+  def initialize
+    @row, @column = 0, 0
+    @rides = []
   end
 
   def rides
-    []
+    @rides
+  end
+
+  def add_ride(ride)
+    @rides << ride
+  end
+
+  def can_ride?(ride)
+    @rides.sum(&:distance) <= ride.earlist_start
   end
 end
