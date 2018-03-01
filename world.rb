451264cba8f7bf5c0a4604488f8ab7bcdf351c3a
Yevhen Shemet <yevhene@gmail.com>
2018-03-01 21:54:26 +0200

    Earliest distance

--- world.rb
+++ world.rb
@@ -20,7 +20,7 @@ class World
   private
 
   def rides_sorted_by_earliest_start
-    @rides.sort_by { |ride| ride.latest_finish - ride.distance }
+    @rides.sort_by { |ride| ride.earliest_start + ride.distance }
   end
 
   def find_car_for(ride)
