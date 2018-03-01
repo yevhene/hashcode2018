cf8c0887ac9d794d320c580726999f10e75f6ce3
Yevhen Shemet <yevhene@gmail.com>
2018-03-01 21:16:34 +0200

    Run

---
+++ run.sh
@@ -0,0 +1,5 @@
+ruby app.rb < in/a_example.in > out/a_example.out
+ruby app.rb < in/b_should_be_easy.in > out/b_should_be_easy.out
+ruby app.rb < in/c_no_hurry.in > out/c_no_hurry.out
+ruby app.rb < in/d_metropolis.in > out/d_metropolis.out
+ruby app.rb < in/e_high_bonus.in > out/e_high_bonus.out
